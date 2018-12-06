a2447b87926d6dd7c251a6ae109802afbc57f8a9
Jason Lillywhite <jason.lillywhite@gmail.com>
2018-12-06 09:38:56 -0800

    Test for equality of bucket capacity rather than comp capacity

--- tests/test_awbm_buckets.py
+++ tests/test_awbm_buckets.py
@@ -6,6 +6,7 @@ class TestBucketCase(unittest.TestCase):
         """Set up a new object to be tested"""
 
         self.a1 = Awbm()
+        self.partial_area_fraction = [0.134, 0.433, 0.433]
 
     def tearDown(self):
         """Destroy the object after running tests"""
@@ -16,7 +17,9 @@ class TestBucketCase(unittest.TestCase):
         """New bucket capacity == user defined array"""
         new_buckets = [57.43, 143.33, 298.49]
         self.a1.set_comp_capacity(new_buckets)
-        self.assertEqual(self.a1.depth_comp_capacity, new_buckets)
+        new_capacity = []
+        for i in range(len(new_buckets)):
+            self.assertEqual(self.a1.buckets.stores[i].capacity, new_buckets[i] * self.partial_area_fraction[i])
 
     #def testBucketOverflow(self):
 
