9d6fa040a93810d66e48d8bcccaaf3013777ab89
Jason Lillywhite <jason.lillywhite@gmail.com>
2019-01-22 15:08:38 -0800

    Incorporated maximum_flow for the network

--- tests/manual_tests.py
+++ tests/manual_tests.py
@@ -1,4 +1,21 @@
 from watershed import Watershed
+import matplotlib.pylab as plt
+import networkx as nx
+from pathlib import Path
+
+data_folder = Path("../data/")
+
+fname = data_folder / "watershed_adj_input"
+#fname = data_folder / "watershed_GML_input.xml"
+
+with open(fname, 'r') as f:
+    content = f.readlines()
+
+g = nx.parse_adjlist(content)
+
+plt.subplot()
+nx.draw(g, with_labels=True)
+plt.show()
 
 w = Watershed()
 w.add_catchment('c2', 'J1')
