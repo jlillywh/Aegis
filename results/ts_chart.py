3f0d71e9980a24d20eae51241ff27dbeeaa4c0b2
jlillywhite <jason.lillywhite@gmail.com>
2019-02-10 19:38:02 -0800

    Add generic 'chart' class that all other types of charts rely on.

--- results/ts_chart.py
+++ results/ts_chart.py
@@ -7,11 +7,10 @@ class TimeSeriesChart(Chart):
     def __init__(self, name):
         Chart.__init__(self, name)
         range = pd.date_range('1/1/2019', periods=365, freq='D')
-        self.series = pd.Series(np.random.rand(1,365), index=range)
+        self.series = pd.Series(np.zeros(len(range)), index=range)
         
     def show(self):
-        x = self.series.values
-        y = np.random.randint(100, size=x.shape)
-        plt.plot(x, y)
+        self.series.plot()
+        plt.show()
     
     
