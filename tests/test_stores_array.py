93824330ee8d071d2119599c9d7ee8da1d477f5a
jlillywhite <jason.lillywhite@gmail.com>
2019-04-02 17:29:42 -0700

    Document controller

--- tests/test_stores_array.py
+++ tests/test_stores_array.py
@@ -1,22 +1,22 @@
 import unittest
 from water_manage.store_array import StoreArray
+from inputs.constants import U
 
-"""To Do:
-    - add test for transferring _quantity from one item to another.
-    """
+
 
 class TestStoresCase(unittest.TestCase):
     def setUp(self):
         """
         Set up a new object to be tested
+        #TODO: add test for transferring _quantity from one item to another.
         """
 
         self.sa = StoreArray(4)
-        inflow = [2.5, 7.8, 23.65, 5.23]
-        outflow = [11.0, 0.0, 2.2, 100.0]
+        inflow = [2.5, 7.8, 23.65, 5.23] * U.m3/U.day
+        outflow = [11.0, 0.0, 2.2, 100.0] * U.m3/U.day
         self.sa.update(inflow, outflow)
 
-        self.total_quantity = 29.25
+        self.total_quantity = 29.25 * U.m3
 
     def tearDown(self):
         """
@@ -29,10 +29,10 @@ class TestStoresCase(unittest.TestCase):
         self.assertEqual(self.sa.total_quantity(), self.total_quantity)
 
     def testOverflow(self):
-	    self.assertEqual(self.sa.total_overflow(), 0.0)
+        self.assertEqual(self.sa.total_overflow(), 0.0 * U.m3/U.day)
 
     def testOutflow(self):
-        self.assertAlmostEqual(self.sa.total_outflow(), 9.93)
+        self.assertAlmostEqual(self.sa.total_outflow(), 9.93 * U.m3/U.day)
 
 if __name__ == '__main__':
     unittest.main()
