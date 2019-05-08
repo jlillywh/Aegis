51e460c17d5bd34ecdb907877be8784909869e73
jlillywhite <jason.lillywhite@gmail.com>
2019-05-08 14:36:52 -0700

    Units interface for datum

--- geometry/datum.py
+++ geometry/datum.py
@@ -1,24 +1,14 @@
 from geometry.point import Point
 
 
-class Datum(Point):
+class Datum:
     """Class for creating datum coordinates
         
         A datum is a reference point that has a location
         in space along with an elevation based on a geodetic datum """
-    def __init__(self, elevation=0, unit='ft'):
-        Point.__init__(self, unit=unit)
-        self._location = (self._x, self._y)
-        self._z = self.to_base_value(elevation)
-
-    @property  # when you do Datum.location, it will call this function
-    def location(self):
-        return self._location
-
-    @location.setter  # when you do Datum.location = x, it will call this function
-    def location(self, x, y):
-        self._x = self.to_base_value(x)
-        self._y = self.to_base_value(y)
-        self._location = (self._x, self._y)
-        
-    
+    def __init__(self, elev=0, lat=40.0, long=101.3, unit='ft'):
+        self.latitude = lat
+        self.longitude = long
+        self.display_unit = unit
+        self._location = (self.latitude, self.longitude)
+        self.elevation = elev
