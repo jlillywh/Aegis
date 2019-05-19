900d84937503cae786172e0e1ac1e6a738bca877
jlillywhite <jason.lillywhite@gmail.com>
2019-05-19 09:52:18 -0700

    Added larger network test

--- water_manage/test_network.py
+++ water_manage/test_network.py
@@ -33,9 +33,26 @@ class TestMyNetwork(unittest.TestCase):
         self.n1.add_junction('J1', 'Sink')
         self.n1.add_source('C1', capacity, 'J1')
         self.n1.add_source('C2', capacity, 'J1')
-        self.n1.draw()
+        # self.n1.draw()
         discharge = self.n1.outflow()
         self.assertEqual(discharge, capacity * 2)
+        
+    def testMultipleJunctions(self):
+        """Add multiple junctions that joins 2 source nodes"""
+        capacity1 = np.random.random()
+        capacity2 = np.random.random()
+        capacity3 = np.random.random()
+        capacity4 = np.random.random()
+        expected_sum = capacity1 + capacity2 + capacity3 + capacity4
+        self.n1.add_junction('J1', 'Sink')
+        self.n1.add_junction('J2', 'J1')
+        self.n1.add_source('C1', capacity1, 'J1')
+        self.n1.add_source('C2', capacity2, 'J1')
+        self.n1.add_source('C3', capacity3, 'J2')
+        self.n1.add_source('C4', capacity4, 'J2')
+        # self.n1.draw()
+        discharge = self.n1.outflow()
+        self.assertEqual(discharge, expected_sum)
 
 
 if __name__ == '__main__':
